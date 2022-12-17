1dcd0f4673f6dfe47937efac846693cac074bbd9
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2022-12-17 09:28:10 +0530

    •Array •Math •Stack

--- Leetcode Problem of the Day/150. Evaluate Reverse Polish Notation.java
+++ Leetcode Problem of the Day/150. Evaluate Reverse Polish Notation.java
@@ -1,4 +1,16 @@
 /*
+PROBLEM STATEMENT :
+
+Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+Valid operators are +, -, *, and /. Each operand may be an integer or another expression.
+
+Note that division between two integers should truncate toward zero.
+
+It is guaranteed that the given RPN expression is always valid. That means the expression would always evaluate to a result,
+  and there will not be any division by zero operation.
+
+
 Example 1:
     Input: tokens = ["2","1","+","3","*"]
     Output: 9
