3a7b91911f8be711ca96bdf5a57a3227afe8cf62
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2023-01-01 19:05:11 +0530

    •HashMap •String ->(Sun 1 Jan)

--- Leetcode Problem of the Day/290. Word Pattern.java
+++ Leetcode Problem of the Day/290. Word Pattern.java
@@ -68,3 +68,26 @@ class Solution {
         return true;
     }
 }
+
+
+//optimize solution :
+
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        Map<Character, String> map = new HashMap<>();
+        String[] animals = s.split(" ");
+        if (pattern.length() != animals.length) return false;
+
+        for (int i = 0; i < pattern.length(); i++) {
+            char c = pattern.charAt(i);
+            String animal = animals[i];
+            if (map.containsKey(c)) {
+                if (!map.get(c).equals(animal)) return false;                
+            } else {
+                if (map.containsValue(animal)) return false;
+                map.put(c, animal);
+            }
+        }
+        return true;
+    }
+}
