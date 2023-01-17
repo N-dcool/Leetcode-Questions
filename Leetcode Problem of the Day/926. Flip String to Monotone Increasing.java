fbbc5ac02023000d3826a181fe64fb6a1f661d83
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2023-01-17 19:17:46 +0530

    •String •DP

---
+++ Leetcode Problem of the Day/926. Flip String to Monotone Increasing.java
@@ -0,0 +1,48 @@
+/*
+A binary string is monotone increasing if it consists of some number of 0's 
+(possibly none), followed by some number of 1's (also possibly none).
+
+You are given a binary string s. You can flip s[i] changing it from 0 to 1 or from 1 to 0.
+
+Return the minimum number of flips to make s monotone increasing.
+
+Example 1:
+    Input: s = "00110"
+    Output: 1
+    Explanation: We flip the last digit to get 00111.
+
+Example 2:
+    Input: s = "010110"
+    Output: 2
+    Explanation: We flip to get 011111, or alternatively 000111.
+
+Example 3:
+    Input: s = "00011000"
+    Output: 2
+    Explanation: We flip to get 00000000.
+ 
+
+Constraints:
+    1 <= s.length <= 105
+    s[i] is either '0' or '1'.
+*/
+
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int m = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '0') {
+                ++m;
+            }
+        }
+        int ans = m;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s.charAt(i) == '0') {
+                ans = Math.min(ans, --m);
+            } else {
+                ++m;
+            }
+        }
+        return ans;
+    }
+}
