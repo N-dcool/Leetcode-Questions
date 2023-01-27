bfb30c3f58aaea5afa7dcc059e0aed38184392f7
NARESH CHOUDHARY <107427909+Nareshchoudhary02@users.noreply.github.com>
2023-01-27 23:30:17 +0530

    •Array •String •DP •DFS •Tries ->(Fri 27 Jan)

---
+++ Leetcode Problem of the Day/472. Concatenated Words.java
@@ -0,0 +1,48 @@
+/*
+Given an array of strings words (without duplicates), return all the concatenated words in the given list of words.
+
+A concatenated word is defined as a string that is comprised entirely of at least two shorter words in the given array.
+
+ 
+
+Example 1:
+    Input: words = ["cat","cats","catsdogcats","dog","dogcatsdog","hippopotamuses","rat","ratcatdogcat"]
+    Output: ["catsdogcats","dogcatsdog","ratcatdogcat"]
+    Explanation: "catsdogcats" can be concatenated by "cats", "dog" and "cats"; 
+    "dogcatsdog" can be concatenated by "dog", "cats" and "dog"; 
+    "ratcatdogcat" can be concatenated by "rat", "cat", "dog" and "cat".
+
+Example 2:
+    Input: words = ["cat","dog","catdog"]
+    Output: ["catdog"]
+
+Constraints:
+    1 <= words.length <= 104
+    1 <= words[i].length <= 30
+    words[i] consists of only lowercase English letters.
+    All the strings of words are unique.
+    1 <= sum(words[i].length) <= 105
+*/
+
+
+
+class Solution {
+    public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        final Set<String> dictionary = new HashSet<>(Arrays.asList(words));
+        final List<String> answer = new ArrayList<>();
+        for (final String word : words) {
+            final int length = word.length();
+            final boolean[] dp = new boolean[length + 1];
+            dp[0] = true;
+            for (int i = 1; i <= length; ++i) {
+                for (int j = (i == length ? 1 : 0); !dp[i] && j < i; ++j) {
+                    dp[i] = dp[j] && dictionary.contains(word.substring(j, i)); 
+                }
+            }
+            if (dp[length]) {
+                answer.add(word);
+            }
+        }
+        return answer;   
+    }
+}
