fb13964c23aee7e6aa82d6bcc165b3cd4bdbc61a
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-03-21 09:55:03 +0530

    •Array •Math ->(Tue 21 Mar)

---
+++ Leetcode Problem of the Day/2348. Number of Zero-Filled Subarrays.java
@@ -0,0 +1,52 @@
+/*
+Given an integer array nums, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+    Input: nums = [1,3,0,0,2,0,0,4]
+    Output: 6
+    Explanation: 
+    There are 4 occurrences of [0] as a subarray.
+    There are 2 occurrences of [0,0] as a subarray.
+    There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+
+Example 2:
+    Input: nums = [0,0,0,2,0,0]
+    Output: 9
+    Explanation:
+    There are 5 occurrences of [0] as a subarray.
+    There are 3 occurrences of [0,0] as a subarray.
+    There is 1 occurrence of [0,0,0] as a subarray.
+    There is no occurrence of a subarray with a size more than 3 filled with 0. Therefore, we return 9.
+
+Example 3:
+    Input: nums = [2,10,2019]
+    Output: 0
+    Explanation: There is no subarray filled with 0. Therefore, we return 0.
+ 
+
+Constraints:
+    1 <= nums.length <= 105
+    -109 <= nums[i] <= 109
+*/
+
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        long subarrayCount = 0;
+        int n = nums.length;
+        for(int i=0; i<n; i++){
+            int count = 0;
+            while(i<n && nums[i]==0){
+                count++;
+                i++;
+            }
+            if(count > 0)
+                subarrayCount += 1l*count*(count+1)/2;
+        }
+        
+        return subarrayCount;
+    }
+}
