0b2953e4c9a99f8838e612696aecbef371adee2c
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-03-31 23:42:42 +0530

    •Array •BinarySearch •Sorting •PrefixSum

---
+++ Weekly Contest 🥷🏻/Weekly Contest 338/2602. Minimum Operations to Make All Array Elements Equal.java
@@ -0,0 +1,71 @@
+/*
+You are given an array nums consisting of positive integers.
+
+You are also given an integer array queries of size m. For the ith query, you want to make all of the elements of nums equal to queries[i]. You can perform the following operation on the array any number of times:
+
+Increase or decrease an element of the array by 1.
+Return an array answer of size m where answer[i] is the minimum number of operations to make all elements of nums equal to queries[i].
+
+Note that after each query the array is reset to its original state.
+
+ 
+Example 1:
+    Input: nums = [3,1,6,8], queries = [1,5]
+    Output: [14,10]
+    Explanation: For the first query we can do the following operations:
+    - Decrease nums[0] 2 times, so that nums = [1,1,6,8].
+    - Decrease nums[2] 5 times, so that nums = [1,1,1,8].
+    - Decrease nums[3] 7 times, so that nums = [1,1,1,1].
+    So the total number of operations for the first query is 2 + 5 + 7 = 14.
+    For the second query we can do the following operations:
+    - Increase nums[0] 2 times, so that nums = [5,1,6,8].
+    - Increase nums[1] 4 times, so that nums = [5,5,6,8].
+    - Decrease nums[2] 1 time, so that nums = [5,5,5,8].
+    - Decrease nums[3] 3 times, so that nums = [5,5,5,5].
+    So the total number of operations for the second query is 2 + 4 + 1 + 3 = 10.
+Example 2:
+    Input: nums = [2,9,6,3], queries = [10]
+    Output: [20]
+    Explanation: We can increase each value in the array to 10. The total number of operations will be 8 + 1 + 4 + 7 = 20.
+ 
+
+Constraints:
+    n == nums.length
+    m == queries.length
+    1 <= n, m <= 105
+    1 <= nums[i], queries[i] <= 109
+*/
+
+class Solution {
+    public List<Long> minOperations(int[] nums, int[] queries) {
+        List<Long> ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+        int n = nums.length;
+        long[] prefix = new long[n + 1];
+        for (int i = 1; i <= n; i++)
+            prefix[i] = prefix[i - 1] + nums[i - 1];
+        for (int x: queries) {
+            int i = binarySearch(x, nums);
+            ans.add(1L * x * (2 * i - n) + prefix[n] - 2 * prefix[i]);
+        }
+        return ans;
+    }
+    
+    public int binarySearch(int num, int[] nums){
+        int l = 0;
+        int r = nums.length;
+        
+        while(l<r){
+            int mid = (l+r)/2;
+            if(nums[mid]==num)
+                return mid;
+            else if(nums[mid]<num)
+                l = mid+1;
+            else
+                r = mid;
+        }
+        
+        return l;
+    }
+}
