b34617632f1eff80ca71f9abcb3cda0ebbf6bf9e
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-04-13 09:17:12 +0530

    •Array •Stack •Simulation ->(Thu 13 Apr)

---
+++ Leetcode Problem of the Day/946. Validate Stack Sequences.java
@@ -0,0 +1,52 @@
+/*
+Given two integer arrays pushed and popped each with distinct values, return true if this could have been the result of a
+sequence of push and pop operations on an initially empty stack, or false otherwise.
+
+ 
+
+Example 1:
+    Input: pushed = [1,2,3,4,5], popped = [4,5,3,2,1]
+    Output: true
+    Explanation: We might do the following sequence:
+    push(1), push(2), push(3), push(4),
+    pop() -> 4,
+    push(5),
+    pop() -> 5, pop() -> 3, pop() -> 2, pop() -> 1
+
+Example 2:
+    Input: pushed = [1,2,3,4,5], popped = [4,3,5,1,2]
+    Output: false
+    Explanation: 1 cannot be popped before 2.
+ 
+
+Constraints:
+    1 <= pushed.length <= 1000
+    0 <= pushed[i] <= 1000
+    All the elements of pushed are unique.
+    popped.length == pushed.length
+    popped is a permutation of pushed.
+*/
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> s = new Stack<>();
+        int n = pushed.length;
+        int j = 0;
+        
+        for(int i=0; i<n; i++){
+            while(j<n && !s.isEmpty() && s.peek() == popped[j]){
+                s.pop();
+                j++;
+            }
+            s.add(pushed[i]);
+            
+        }
+        
+        while(j<n && s.peek() == popped[j]){
+            s.pop();
+            j++;
+        }
+                
+        return s.isEmpty();
+    }
+}
