ceeadf4fa54d2b50ffc2c0b75705477304a9d11a
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-04-14 11:16:41 +0530

    •Array •Math •Matrix •NumberTheory

---
+++ Weekly Contest 🥷🏻/Weekly Contest 340/2614. Prime In Diagonal.java
@@ -0,0 +1,62 @@
+/*
+You are given a 0-indexed two-dimensional integer array nums.
+
+Return the largest prime number that lies on at least one of the diagonals of nums. In case, no prime is present on any of the diagonals, return 0.
+
+Note that:
+
+An integer is prime if it is greater than 1 and has no positive integer divisors other than 1 and itself.
+An integer val is on one of the diagonals of nums if there exists an integer i for which nums[i][i] = val or
+ an i for which nums[i][nums.length - i - 1] = val.
+
+In the above diagram, one diagonal is [1,5,9] and another diagonal is [3,5,7].
+
+ 
+Example 1:
+    Input: nums = [[1,2,3],[5,6,7],[9,10,11]]
+    Output: 11
+    Explanation: The numbers 1, 3, 6, 9, and 11 are the only numbers present on at least one of the diagonals. Since 11 is the largest prime, we return 11.
+
+Example 2:
+    Input: nums = [[1,2,3],[5,17,7],[9,11,10]]
+    Output: 17
+    Explanation: The numbers 1, 3, 9, 10, and 17 are all present on at least one of the diagonals. 17 is the largest prime, so we return 17.
+ 
+
+Constraints:
+    1 <= nums.length <= 300
+    nums.length == numsi.length
+    1 <= nums[i][j] <= 4*106
+*/
+
+class Solution {
+    public int diagonalPrime(int[][] nums) {
+        int ans = 0;
+        int n = nums.length;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(i==j && isPrime(nums[i][j])){
+                    ans = Math.max(ans, nums[i][j]);
+                }
+                if(i+j==n-1 && isPrime(nums[i][j])){
+                    ans = Math.max(ans, nums[i][j]);
+                }
+            }
+        }
+        
+        return ans;
+    }
+    
+    public boolean isPrime(int num){
+        if (num <= 1) {
+               return false;
+           }
+           for (int i = 2; i <= Math.sqrt(num); i++) {
+               if (num % i == 0) {
+                   return false;
+               }
+           }
+           return true;
+    }
+}
