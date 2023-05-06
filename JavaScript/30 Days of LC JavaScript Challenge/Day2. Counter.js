16c9f64fee9f1896c6fef5e36cb4c07a729cbc76
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-05-06 23:13:17 +0530

    •counting ->(Sat 6 May)

--- JavaScript/30 Days of LC JavaScript Challenge/Day2. Counter.js
+++ JavaScript/30 Days of LC JavaScript Challenge/Day2. Counter.js
@@ -3,7 +3,6 @@ Given an integer n, return a counter function.
 This counter function initially returns n and then returns 1 more than the previous value
 every subsequent time it is called (n, n + 1, n + 2, etc).
 
-
 Example 1:
     Input: 
     n = 10 
