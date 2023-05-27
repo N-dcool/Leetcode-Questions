888453f9d9ea7199029804cb1ade6a48e4655425
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-05-27 21:57:36 +0530

    •ArrayPrototype ->(Sat 27 May)

--- JavaScript/30 Days of LC JavaScript Challenge/23. Array Prototype Last.js
+++ JavaScript/30 Days of LC JavaScript Challenge/23. Array Prototype Last.js
@@ -2,7 +2,6 @@
 Write code that enhances all arrays such that you can call the array.last() method on any array and it will 
 return the last element. If there are no elements in the array, it should return -1.
  
-
 Example 1:
     Input: nums = [1,2,3]
     Output: 3
