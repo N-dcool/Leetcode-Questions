192b01b09bc39216e24859632f78dcbb95f07f39
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-06-01 22:26:40 +0530

    •Array •BFS •Matrix ->(Thu 1 Jun)

---
+++ Leetcode Problem of the Day/1091. Shortest Path in Binary Matrix.java
@@ -0,0 +1,86 @@
+/*
+Given an n x n binary matrix grid, return the length of the shortest clear path in the matrix. If there is no clear path, return -1.
+
+A clear path in a binary matrix is a path from the top-left cell (i.e., (0, 0)) to the bottom-right cell (i.e., (n - 1, n - 1)) such that:
+
+All the visited cells of the path are 0.
+All the adjacent cells of the path are 8-directionally connected (i.e., they are different and they share an edge or a corner).
+The length of a clear path is the number of visited cells of this path.
+
+ 
+
+Example 1:
+    Input: grid = [[0,1],[1,0]]
+    Output: 2
+
+Example 2:
+    Input: grid = [[0,0,0],[1,1,0],[1,1,0]]
+    Output: 4
+
+Example 3:
+    Input: grid = [[1,0,0],[1,1,0],[1,1,0]]
+    Output: -1
+ 
+
+Constraints:
+    n == grid.length
+    n == grid[i].length
+    1 <= n <= 100
+    grid[i][j] is 0 or 1
+*/
+
+class Solution {
+    
+    record Pair(int first, int second){}
+    
+    int[][] dirs = new int[][]{{1,1},{1,0},{0,1},{-1,0},{0,-1},{1,-1},{-1,1},{-1,-1}};
+    public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        if(grid[0][0] == 1 || grid[n-1][m-1] == 1)
+            return -1;
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(0,0));
+        grid[0][0] = 2;
+        int dis = 0;
+        
+        while(!q.isEmpty()){
+            int size = q.size();
+            
+            while(size-- > 0){
+                Pair cur = q.poll();
+                int x = cur.first;
+                int y = cur.second;
+
+                if(x==n-1 && y==m-1)
+                    return dis+1;
+
+                for(int[] dir : dirs){
+                    int i = x + dir[0];
+                    int j = y + dir[1];
+
+                    if(i>=0 && j>=0 && i<n && j<m && grid[i][j] == 0){
+                        q.offer(new Pair(i, j));
+                        grid[i][j] = 2;
+                    }
+                }
+            }
+            
+            dis++;
+        }
+        
+        return -1;
+    }
+}
+
+/*
+for(int[] g : grid){
+                for(int a : g)
+                    System.out.print(a + " ");
+                System.out.println();
+            }
+            
+            System.out.println(q + " "+dis);
+*/
