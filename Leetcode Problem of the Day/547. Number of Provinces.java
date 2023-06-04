f9261af767c417e4fc859c6ebfd3e2af9f3bff14
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-06-04 22:48:13 +0530

    •DFS •BFS •UnionFind •Graph ->(Sun 4 Jun)

---
+++ Leetcode Problem of the Day/547. Number of Provinces.java
@@ -0,0 +1,55 @@
+/*
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected 
+directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+ 
+
+Example 1:
+    Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+    Output: 2
+
+Example 2:
+    Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+    Output: 3
+ 
+
+Constraints:
+    1 <= n <= 200
+    n == isConnected.length
+    n == isConnected[i].length
+    isConnected[i][j] is 1 or 0.
+    isConnected[i][i] == 1
+    isConnected[i][j] == isConnected[j][i]
+*/
+
+class Solution {
+    public void dfs(int node, int[][] isConnected, boolean[] visit) {
+        visit[node] = true;
+        for (int i = 0; i < isConnected.length; i++) {
+            if (isConnected[node][i] == 1 && !visit[i]) {
+                dfs(i, isConnected, visit);
+            }
+        }
+    }
+
+    public int findCircleNum(int[][] isConnected) {
+        int n = isConnected.length;
+        int numberOfComponents = 0;
+        boolean[] visit = new boolean[n];
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                numberOfComponents++;
+                dfs(i, isConnected, visit);
+            }
+        }
+
+        return numberOfComponents;
+    }
+}
