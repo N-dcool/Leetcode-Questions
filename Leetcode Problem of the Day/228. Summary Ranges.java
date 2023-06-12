c8d7dbface1b9ae7d631a79779ae06dac08729ad
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-06-12 17:39:52 +0530

    •Array ->(Mon 12 Jun)

---
+++ Leetcode Problem of the Day/228. Summary Ranges.java
@@ -0,0 +1,73 @@
+/*
+You are given a sorted unique integer array nums.
+
+A range [a,b] is the set of all integers from a to b (inclusive).
+
+Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is
+covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+Each range [a,b] in the list should be output as:
+
+"a->b" if a != b
+"a" if a == b
+ 
+
+Example 1:
+    Input: nums = [0,1,2,4,5,7]
+    Output: ["0->2","4->5","7"]
+    Explanation: The ranges are:
+    [0,2] --> "0->2"
+    [4,5] --> "4->5"
+    [7,7] --> "7"
+
+Example 2:
+      Input: nums = [0,2,3,4,6,8,9]
+      Output: ["0","2->4","6","8->9"]
+      Explanation: The ranges are:
+      [0,0] --> "0"
+      [2,4] --> "2->4"
+      [6,6] --> "6"
+      [8,9] --> "8->9"
+
+
+Constraints:
+    0 <= nums.length <= 20
+    -231 <= nums[i] <= 231 - 1
+    All the values of nums are unique.
+    nums is sorted in ascending order.
+*/
+
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        int n = nums.length;
+        List<String> ans = new ArrayList<>();
+        
+        if(n==0)
+            return ans;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        int start = nums[0];
+        
+        for(int i=1; i<n; i++){
+            if(nums[i-1]+1 == nums[i]) 
+                continue;
+            else if(nums[i-1]+1 < nums[i]){
+                if(start == nums[i-1])
+                    ans.add(start +"");
+                else
+                    ans.add(start + "->" + nums[i-1]);
+                    
+                start = nums[i];
+            }
+        }
+        
+        if(start == nums[n-1])
+            ans.add(start+"");
+        else
+            ans.add(start +"->"+ nums[n-1]);
+        
+        return ans;
+        
+    }
+}
