91aec4b4fd2a438715529548344385050687d072
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-06-15 21:11:48 +0530

    •Tree •BFS •DFS •BinaryTree ->(Thu 15 Jun)

---
+++ Leetcode Problem of the Day/1161. Maximum Level Sum of a Binary Tree.java
@@ -0,0 +1,74 @@
+/*
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+
+Example 1:
+    Input: root = [1,7,0,7,-8,null,null]
+    Output: 2
+    Explanation: 
+    Level 1 sum = 1.
+    Level 2 sum = 7 + 0 = 7.
+    Level 3 sum = 7 + -8 = -1.
+    So we return the level with the maximum sum which is level 2.
+
+Example 2:
+    Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+    Output: 2
+ 
+
+Constraints:
+    The number of nodes in the tree is in the range [1, 104].
+    -105 <= Node.val <= 105
+*/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxLevelSum(TreeNode root) {
+        int maxSum = Integer.MIN_VALUE;
+        int ans = 0, level = 0;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty()) {
+            level++;
+            int sumAtCurrentLevel = 0;
+            // Iterate over all the nodes in the current level.
+            for (int sz = q.size(); sz > 0; --sz) {
+                TreeNode node = q.poll();
+                sumAtCurrentLevel += node.val;
+
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+
+            if (maxSum < sumAtCurrentLevel) {
+                maxSum = sumAtCurrentLevel;
+                ans = level;
+            }
+        }
+
+        return ans;
+    }
+}
