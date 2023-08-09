c070855bc630eb8f5619d42abdc620b33bf7d08d
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-08-09 18:19:08 +0530

    •Array •BinarySearch •Greedy

---
+++ Weekly Contest 🥷🏻/Weekly Contest 340/2616. Minimize the Maximum Difference of Pairs.java
@@ -0,0 +1,58 @@
+/*
+You are given a 0-indexed integer array nums and an integer p. Find p pairs of indices of nums such that the maximum difference
+amongst all the pairs is minimized. Also, ensure no index appears more than once amongst the p pairs.
+
+Note that for a pair of elements at the index i and j, the difference of this pair is |nums[i] - nums[j]|, where |x| represents the absolute value of x.
+
+Return the minimum maximum difference among all p pairs. We define the maximum of an empty set to be zero.
+
+
+Example 1:
+    Input: nums = [10,1,2,7,1,3], p = 2
+    Output: 1
+    Explanation: The first pair is formed from the indices 1 and 4, and the second pair is formed from the indices 2 and 5. 
+    The maximum difference is max(|nums[1] - nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1. Therefore, we return 1.
+
+Example 2:
+    Input: nums = [4,2,1,2], p = 1
+    Output: 0
+    Explanation: Let the indices 1 and 3 form a pair. The difference of that pair is |2 - 2| = 0, which is the minimum we can attain.
+ 
+
+Constraints:
+    1 <= nums.length <= 105
+    0 <= nums[i] <= 109
+    0 <= p <= (nums.length)/2
+*/
+
+class Solution {
+    public int minimizeMax(int[] nums, int p) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        
+        int l = 0;
+        int r = nums[n-1] - nums[0];
+        
+        while(l < r){
+            int mid = l + (r-l)/2;
+            if(isPossible(nums, p, mid))
+                r = mid;
+            else
+                l = mid+1;
+        }
+        
+        return l;
+    }
+    
+    public boolean isPossible(int[] nums, int p, int diff){
+        
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] - nums[i-1] <= diff){
+                p--;
+                i++;
+            }
+        }
+        
+        return p<=0;
+    }
+}
