83427c27d908a6e5e8428b3b0ed48f0ec01c3504
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-08-14 14:35:57 +0530

    •Array •Divide&Conquer •Sorting •PriorityQueue •QuickSort ->(Mon 14 Aug)

---
+++ Leetcode Problem of the Day/215. Kth Largest Element in an Array.java
@@ -0,0 +1,35 @@
+/*
+Given an integer array nums and an integer k, return the kth largest element in the array.
+
+Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+Can you solve it without sorting?
+
+ 
+
+Example 1:
+    Input: nums = [3,2,1,5,6,4], k = 2
+    Output: 5
+
+Example 2:
+    Input: nums = [3,2,3,1,2,4,5,5,6], k = 4
+    Output: 4
+ 
+
+Constraints:
+    1 <= k <= nums.length <= 105
+    -104 <= nums[i] <= 104
+*/
+
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        for(int num : nums){
+            pq.add(num);
+            if(pq.size() > k) pq.poll();
+        }
+        
+        return pq.peek();
+    }
+}
