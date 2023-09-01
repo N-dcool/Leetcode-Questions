7c6af8e529ec32d66a4a264dd61a7fa2e56e16eb
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-09-01 11:41:02 +0530

    •Dp •BitManipulation ->(Fri 1 Sep)

---
+++ Leetcode Problem of the Day/338. Counting Bits.java
@@ -0,0 +1,65 @@
+/*
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the
+binary representation of i.
+
+ 
+
+Example 1:
+    Input: n = 2
+    Output: [0,1,1]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+
+Example 2:
+    Input: n = 5
+    Output: [0,1,1,2,1,2]
+    Explanation:
+    0 --> 0
+    1 --> 1
+    2 --> 10
+    3 --> 11
+    4 --> 100
+    5 --> 101
+ 
+
+Constraints:
+    0 <= n <= 105
+ 
+Follow up:
+    It is very easy to come up with a solution with a runtime of O(n log n). Can you do it in linear time O(n) and possibly in a single pass?
+    Can you do it without using any built-in function (i.e., like __builtin_popcount in C++)?
+*/
+
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n+1];
+        
+        for(int i=0; i<=Math.min(1,n); i++)
+            res[i] = i;
+        int prev = 1;
+        for(int i=2; i<=n; i++){
+            if((i&(i-1))==0){
+                res[i] = 1;
+                prev = 1;
+            }else{
+                res[i] = 1 + res[prev++];
+            }
+            
+        }
+        
+        return res;
+    }
+}
+/*
+0 = 0
+1 = 1
+2 = 10
+3 = 11
+4 = 100
+5 = 101
+6 = 110
+7 = 111
+8 = 1000
+*/
