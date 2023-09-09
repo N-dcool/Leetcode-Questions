528166d33837a40d89234bc083d17280b3d46606
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-09-09 12:32:32 +0530

    •Array •DP ->(Sat 9 Sep)

---
+++ Leetcode Problem of the Day/377. Combination Sum IV.java
@@ -0,0 +1,57 @@
+/*
+Given an array of distinct integers nums and a target integer target, return the number of possible combinations that add up to target.
+
+The test cases are generated so that the answer can fit in a 32-bit integer.
+
+Example 1:
+    Input: nums = [1,2,3], target = 4
+    Output: 7
+    Explanation:
+    The possible combination ways are:
+    (1, 1, 1, 1)
+    (1, 1, 2)
+    (1, 2, 1)
+    (1, 3)
+    (2, 1, 1)
+    (2, 2)
+    (3, 1)
+    Note that different sequences are counted as different combinations.
+
+Example 2:
+    Input: nums = [9], target = 3
+    Output: 0
+ 
+
+Constraints:
+    1 <= nums.length <= 200
+    1 <= nums[i] <= 1000
+    All the elements of nums are unique.
+    1 <= target <= 1000
+ 
+Follow up: What if negative numbers are allowed in the given array? How does it change the problem? What limitation we need to add to the question to 
+           allow negative numbers?
+*/
+
+
+class Solution {
+    Integer[] dp;
+    public int combinationSum4(int[] nums, int target) {
+        dp = new Integer[target+1];
+        
+        return solve(nums.length, nums, target);
+    }
+    
+    public int solve(int n, int[] nums, int target){
+        if(target==0) return 1;
+        if(dp[target] != null)
+            return dp[target];
+        int res = 0;
+        for(int i=0; i<n; i++){
+            if(target >= nums[i])
+                res += solve(n, nums, target-nums[i]);
+        }
+        
+        return dp[target] = res;
+    }
+    
+}
