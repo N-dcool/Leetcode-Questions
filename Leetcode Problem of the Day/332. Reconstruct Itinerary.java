23148e1be4fe63cc0e19f6850d95dc3586afaf49
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-09-14 14:21:04 +0530

    •DFS •Graph •PriorityQueue •EulerianCircuit ->(Thu 14 Sep)

---
+++ Leetcode Problem of the Day/332. Reconstruct Itinerary.java
@@ -0,0 +1,59 @@
+/*
+You are given a list of airline tickets where tickets[i] = [fromi, toi] represent the departure and the arrival airports of one flight. 
+Reconstruct the itinerary in order and return it.
+
+All of the tickets belong to a man who departs from "JFK", thus, the itinerary must begin with "JFK". If there are multiple valid itineraries, 
+you should return the itinerary that has the smallest lexical order when read as a single string.
+
+For example, the itinerary ["JFK", "LGA"] has a smaller lexical order than ["JFK", "LGB"].
+You may assume all tickets form at least one valid itinerary. You must use all the tickets once and only once.
+
+ 
+
+Example 1:
+    Input: tickets = [["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]
+    Output: ["JFK","MUC","LHR","SFO","SJC"]
+
+Example 2:
+    Input: tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
+    Output: ["JFK","ATL","JFK","SFO","ATL","SFO"]
+    Explanation: Another possible reconstruction is ["JFK","SFO","ATL","JFK","ATL","SFO"] but it is larger in lexical order.
+ 
+
+Constraints:
+    1 <= tickets.length <= 300
+    tickets[i].length == 2
+    fromi.length == 3
+    toi.length == 3
+    fromi and toi consist of uppercase English letters.
+    fromi != toi
+*/
+
+class Solution {
+    List<String> res;
+    public List<String> findItinerary(List<List<String>> tickets) {
+        res = new ArrayList<>();
+        HashMap<String, PriorityQueue<String>> graph = new HashMap<>();
+        
+        for(List<String> t : tickets){
+            graph.computeIfAbsent(t.get(0), k-> new PriorityQueue<>()).add(t.get(1));
+        }
+
+        solve("JFK", graph);
+        
+        Collections.reverse(res);
+        
+        return res;
+    }
+    
+    public void solve(String node, HashMap<String, PriorityQueue<String>> map){
+        
+        if(map.containsKey(node)){
+            while(!map.get(node).isEmpty()){
+                solve(map.get(node).poll(), map);
+            }
+        }
+        res.add(node);
+    }
+    
+}
