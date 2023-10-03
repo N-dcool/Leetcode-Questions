a2a2efaf596f54469c5febc44a8c99e47b24ce68
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-10-03 10:05:34 +0530

    •Array •HashTable •Math •Counting ->(Tue 3 Oct)

---
+++ Leetcode Problem of the Day/1512. Number of Good Pairs.java
@@ -0,0 +1,44 @@
+/*
+Given an array of integers nums, return the number of good pairs.
+
+A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+
+ 
+Example 1:
+    Input: nums = [1,2,3,1,1,3]
+    Output: 4
+    Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+Example 2:
+    Input: nums = [1,1,1,1]
+    Output: 6
+    Explanation: Each pair in the array are good.
+
+Example 3:
+    Input: nums = [1,2,3]
+    Output: 0
+ 
+
+Constraints:
+    1 <= nums.length <= 100
+    1 <= nums[i] <= 100
+*/
+
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int num : nums) map.put(num, map.getOrDefault(num,0)+1);
+        
+        long res = 0;
+        for(Map.Entry<Integer,Integer> e : map.entrySet()){
+            int val = e.getValue();
+            res += countPairs(val);
+        }
+        
+        return (int)res;
+    }
+    
+    public int countPairs(int n){
+        return n*(n-1)/2;
+    }
+}
