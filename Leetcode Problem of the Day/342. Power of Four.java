fa820104960f28c0faef0a341606c686884532e4
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-10-23 17:53:45 +0530

    •Math •BitManipulation •Recursion ->(Mon 23 Oct)

---
+++ Leetcode Problem of the Day/342. Power of Four.java
@@ -0,0 +1,33 @@
+/*
+Given an integer n, return true if it is a power of four. Otherwise, return false.
+
+An integer n is a power of four, if there exists an integer x such that n == 4x.
+
+ 
+Example 1:
+    Input: n = 16
+    Output: true
+
+Example 2:
+    Input: n = 5
+    Output: false
+
+Example 3:
+    Input: n = 1
+    Output: true
+ 
+
+Constraints:
+    -231 <= n <= 231 - 1
+ 
+*/
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        // logn = x log4 => x = log n / log 4 => x = log4(n);
+        
+        double ans = Math.log(n)/Math.log(4);
+        
+        return ans == (int)ans;
+    }
+}
