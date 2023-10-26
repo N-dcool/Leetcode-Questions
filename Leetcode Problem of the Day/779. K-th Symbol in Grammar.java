9cba5587816d23b9b55a57744754d888d860b902
NARESH CHOUDHARY <107427909+N-dcool@users.noreply.github.com>
2023-10-26 22:40:28 +0530

    •Math •BitManipulation •Recurssion ->(Thu 26 Oct)

---
+++ Leetcode Problem of the Day/779. K-th Symbol in Grammar.java
@@ -0,0 +1,61 @@
+/*
+We build a table of n rows (1-indexed). We start by writing 0 in the 1st row. Now in every subsequent row, we look at the previous row 
+and replace each occurrence of 0 with 01, and each occurrence of 1 with 10.
+
+For example, for n = 3, the 1st row is 0, the 2nd row is 01, and the 3rd row is 0110.
+Given two integer n and k, return the kth (1-indexed) symbol in the nth row of a table of n rows.
+
+ 
+
+Example 1:
+    Input: n = 1, k = 1
+    Output: 0
+    Explanation: row 1: 0
+
+Example 2:
+    Input: n = 2, k = 1
+    Output: 0
+    Explanation: 
+    row 1: 0
+    row 2: 01
+
+Example 3:
+    Input: n = 2, k = 2
+    Output: 1
+    Explanation: 
+    row 1: 0
+    row 2: 01
+ 
+
+Constraints:
+    1 <= n <= 30
+    1 <= k <= 2n - 1
+*/
+
+class Solution {
+    public int kthGrammar(int n, int k) {
+        int cur = 0;
+        int left = 0;
+        int right = (int)Math.pow(2,n-1);
+    
+        while(n-->1){
+            int mid = (left+right)/2;
+            if(k<=mid)
+                right = mid;
+            else{
+                left = mid+1;
+                cur = cur==0 ? 1:0;
+            }
+        }
+    
+        return cur;
+    }
+}
+/*
+0
+01
+0110
+01101001
+0110100110010110
+
+*/
