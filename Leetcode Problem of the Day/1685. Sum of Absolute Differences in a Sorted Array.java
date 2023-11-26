9feee0beba5155736aaeaa27bd359faa14073970
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2023-11-26 23:01:47 +0530

    •Array •Math •PrefixSum ->(Sat 25 Nov)

---
+++ Leetcode Problem of the Day/1685. Sum of Absolute Differences in a Sorted Array.java
@@ -0,0 +1,56 @@
+/*
+You are given an integer array nums sorted in non-decreasing order.
+
+Build and return an integer array result with the same length as nums such that result[i] is equal to the summation of absolute
+differences between nums[i] and all the other elements in the array.
+
+In other words, result[i] is equal to sum(|nums[i]-nums[j]|) where 0 <= j < nums.length and j != i (0-indexed).
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,5]
+Output: [4,3,5]
+Explanation: Assuming the arrays are 0-indexed, then
+result[0] = |2-2| + |2-3| + |2-5| = 0 + 1 + 3 = 4,
+result[1] = |3-2| + |3-3| + |3-5| = 1 + 0 + 2 = 3,
+result[2] = |5-2| + |5-3| + |5-5| = 3 + 2 + 0 = 5.
+Example 2:
+
+Input: nums = [1,4,6,8,10]
+Output: [24,15,13,15,21]
+ 
+
+Constraints:
+
+2 <= nums.length <= 105
+1 <= nums[i] <= nums[i + 1] <= 104
+*/
+
+class Solution {
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        
+        long total = 0;
+        for(int num : nums) total += num;
+        
+        long leftSum = 0;
+        for(int i=0; i<n; i++){
+            long rightSum = total - leftSum - nums[i];
+            
+            int leftLen = i;
+            int rightLen = n-1-i;
+            
+            long left = nums[i]*leftLen - leftSum;
+            long right = rightSum - nums[i]*rightLen;
+            
+            res[i] = (int)(left + right);
+            
+            leftSum += nums[i];
+        }
+        
+        return res;
+    }
+}
