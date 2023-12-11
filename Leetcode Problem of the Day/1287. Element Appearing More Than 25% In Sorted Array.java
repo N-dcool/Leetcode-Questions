51e7751ccebe219f50c86f13e74b99e882eb0fdd
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2023-12-11 09:04:17 +0530

    •Array ->(Mon 11 Dec)

---
+++ Leetcode Problem of the Day/1287. Element Appearing More Than 25% In Sorted Array.java
@@ -0,0 +1,34 @@
+/*
+Given an integer array sorted in non-decreasing order, there is exactly one integer in the array that occurs 
+more than 25% of the time, return that integer.
+
+ 
+
+Example 1:
+
+Input: arr = [1,2,2,6,6,6,6,7,10]
+Output: 6
+Example 2:
+
+Input: arr = [1,1]
+Output: 1
+ 
+
+Constraints:
+
+1 <= arr.length <= 104
+0 <= arr[i] <= 105
+*/
+
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int size = arr.length / 4;
+        for (int i = 0; i < arr.length - size; i++) {
+            if (arr[i] == arr[i + size]) {
+                return arr[i];
+            }
+        }
+        
+        return -1;
+    }
+}
