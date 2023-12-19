e3b12cc062a3168efd94afc07c5e0672ea48472f
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2023-12-19 22:07:57 +0530

    •Array •Matrix ->(Tue 19 Dec)

---
+++ Leetcode Problem of the Day/661. Image Smoother.java
@@ -0,0 +1,72 @@
+/*
+An image smoother is a filter of the size 3 x 3 that can be applied to each cell of an image by rounding down the average of the cell and the eight surrounding cells (i.e., the average of the nine cells in the blue smoother). If one or more of the surrounding cells of a cell is not present, we do not consider it in the average (i.e., the average of the four cells in the red smoother).
+
+
+Given an m x n integer matrix img representing the grayscale of an image, return the image after applying the smoother on each cell of it.
+
+ 
+
+Example 1:
+
+
+Input: img = [[1,1,1],[1,0,1],[1,1,1]]
+Output: [[0,0,0],[0,0,0],[0,0,0]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor(3/4) = floor(0.75) = 0
+For the points (0,1), (1,0), (1,2), (2,1): floor(5/6) = floor(0.83333333) = 0
+For the point (1,1): floor(8/9) = floor(0.88888889) = 0
+Example 2:
+
+
+Input: img = [[100,200,100],[200,50,200],[100,200,100]]
+Output: [[137,141,137],[141,138,141],[137,141,137]]
+Explanation:
+For the points (0,0), (0,2), (2,0), (2,2): floor((100+200+200+50)/4) = floor(137.5) = 137
+For the points (0,1), (1,0), (1,2), (2,1): floor((200+200+50+200+100+100)/6) = floor(141.666667) = 141
+For the point (1,1): floor((50+200+200+200+200+100+100+100+100)/9) = floor(138.888889) = 138
+ 
+
+Constraints:
+
+m == img.length
+n == img[i].length
+1 <= m, n <= 200
+0 <= img[i][j] <= 255
+*/
+
+class Solution {
+    public int[][] imageSmoother(int[][] img) {
+        // Save the dimensions of the image.
+        int m = img.length;
+        int n = img[0].length;
+
+        // Create a new image of the same dimension as the input image.
+        int[][] smoothImg = new int[m][n];
+
+        // Iterate over the cells of the image.
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // Initialize the sum and count 
+                int sum = 0;
+                int count = 0;
+
+                // Iterate over all plausible nine indices.
+                for (int x = i - 1; x <= i + 1; x++) {
+                    for (int y = j - 1; y <= j + 1; y++) {
+                        // If the indices form valid neighbor
+                        if (0 <= x && x < m && 0 <= y && y < n) {
+                            sum += img[x][y];
+                            count += 1;
+                        }
+                    }
+                }
+
+                // Store the rounded down value in smoothImg[i][j].
+                smoothImg[i][j] = sum / count;
+            }
+        }
+
+        // Return the smooth image.
+        return smoothImg;
+    }
+}
