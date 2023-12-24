101b9547e91e54107aaf22a95f4121b915b2f032
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2023-12-24 22:39:53 +0530

    •String ->(Sun 24 Dec)

---
+++ Leetcode Problem of the Day/1758. Minimum Changes To Make Alternating Binary String.java
@@ -0,0 +1,58 @@
+/*
+You are given a string s consisting only of the characters '0' and '1'. In one operation, you can change any '0' to '1' or vice versa.
+
+The string is called alternating if no two adjacent characters are equal. For example, the string "010" is alternating, while the string "0100" is not.
+
+Return the minimum number of operations needed to make s alternating.
+
+ 
+
+Example 1:
+
+Input: s = "0100"
+Output: 1
+Explanation: If you change the last character to '1', s will be "0101", which is alternating.
+Example 2:
+
+Input: s = "10"
+Output: 0
+Explanation: s is already alternating.
+Example 3:
+
+Input: s = "1111"
+Output: 2
+Explanation: You need two operations to reach "0101" or "1010".
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s[i] is either '0' or '1'.
+*/
+
+class Solution {
+    public int minOperations(String s) {
+        int one = 0;
+        int zero = 0;
+        
+        boolean oneB = true;
+        boolean zeroB = true;
+        
+        for(char c : s.toCharArray()){
+            if(c=='1'){
+                if(zeroB) zero++;
+                if(!oneB) one++;
+            }
+            else{
+                if(!zeroB) zero++;
+                if(oneB) one++;
+            }
+            
+            oneB = !oneB;
+            zeroB = !zeroB;
+        }
+        
+        return Math.min(one,zero);
+        
+    }
+}
