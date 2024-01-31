54f49bcb0e50e3fafabe9c4939ac9ac549ec9a3e
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2024-01-31 23:16:16 +0530

    •Array •Stack ->(Wed 31 Jan)

---
+++ Leetcode Problem of the Day/739. Daily_Temperatures.java
@@ -0,0 +1,40 @@
+/*
+Given an array of integers temperatures represents the daily temperatures, return an array answer such that answer[i] is the number of days you have to wait after the ith day to get a warmer temperature. If there is no future day for which this is possible, keep answer[i] == 0 instead.
+
+ 
+
+Example 1:
+
+Input: temperatures = [73,74,75,71,69,72,76,73]
+Output: [1,1,4,2,1,1,0,0]
+Example 2:
+
+Input: temperatures = [30,40,50,60]
+Output: [1,1,1,0]
+Example 3:
+
+Input: temperatures = [30,60,90]
+Output: [1,1,0]
+ 
+
+Constraints:
+
+1 <= temperatures.length <= 105
+30 <= temperatures[i] <= 100
+*/
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] stack = new int[temperatures.length];
+        int top = -1;
+        int[] ret = new int[temperatures.length];
+        for(int i = 0; i < temperatures.length; i++) {
+            while(top > -1 && temperatures[i] > temperatures[stack[top]]) {
+                int idx = stack[top--];
+                ret[idx] = i - idx;
+            }
+            stack[++top] = i;
+        }
+        return ret;
+    }
+}
