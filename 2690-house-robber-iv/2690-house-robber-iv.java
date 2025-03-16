1f81e47492ef4a1267ea248d30a66de29d97566f
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2025-03-16 08:49:08 +0530

    Time: 17 ms (97.25%), Space: 65.1 MB (14.25%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.java
+++ 2690-house-robber-iv/2690-house-robber-iv.java
@@ -1,31 +1,43 @@
 class Solution {
-    private boolean canStealKHouses(int[] nums, int k, int capability) {
-        int count = 0;
-        int i = 0;
-        while (i < nums.length) {
-            if (nums[i] <= capability) {
-                count++;
-                i += 2;
-            } else {
-                i++;
-            }
-        }
-        return count >= k;
-    }
-    
     public int minCapability(int[] nums, int k) {
-        int left = Arrays.stream(nums).min().getAsInt();
-        int right = Arrays.stream(nums).max().getAsInt();
-        
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canStealKHouses(nums, k, mid)) {
-                right = mid;
+        int n = nums.length;
+        int left = Integer.MAX_VALUE;
+        int right = 1;
+
+        for(int num : nums){
+            left = Math.min(left, num);
+            right = Math.max(right, num);
+        }
+
+        int ans = left;
+
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            // System.out.println("left : " + left +" right : " + right); 
+            if(isPossible(nums, mid, k)){
+                ans = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        
-        return left;
+
+        return ans;
     }
+
+    public boolean isPossible(int[] nums, int capability, int min){
+        
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] <= capability){
+                // System.out.println(nums[i] +" "+ capability);
+                min--;
+                i++;
+            }
+            if(min == 0) return true;
+        }
+        
+        return false;
+    }
+
+
 }
