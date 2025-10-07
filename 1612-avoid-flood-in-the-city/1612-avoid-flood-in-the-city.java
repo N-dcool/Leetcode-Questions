096675c98efc7429198b0a785d5139329537ec03
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2025-10-07 21:24:22 +0530

    Time: 66 ms (62.56%), Space: 62.3 MB (46.58%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -1,5 +1,32 @@
 class Solution {
     public int[] avoidFlood(int[] rains) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] ans = new int[rains.length];
+        Arrays.fill(ans, 1);
+        TreeSet<Integer> st = new TreeSet<>();
+        int count = 0;
+        for(int i = 0; i < rains.length; i++) {
+            if(rains[i] == 0) {
+                st.add(i);
+            }
+            else {
+                ans[i] = -1;
+                if(map.containsKey(rains[i])) {
+                    Integer it = st.ceiling(map.get(rains[i]));
+                    if(it == null) return new int[0];
+                    ans[it] = rains[i];
+                    st.remove(it);
+                }
+                map.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
+
+/*
+class Solution {
+    public int[] avoidFlood(int[] rains) {
         int n = rains.length;
         HashMap<Integer,Integer> map = new HashMap<>();
         
@@ -60,3 +87,4 @@ class Solution {
         return res;
     }
 }
+*/
