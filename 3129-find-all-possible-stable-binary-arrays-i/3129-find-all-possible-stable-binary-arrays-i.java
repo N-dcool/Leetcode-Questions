82fc3145a18d8c3553ad58d83bf7445fd8635e29
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-03-09 21:51:56 +0530

    Time: 20 ms (86.84%), Space: 47.8 MB (42.11%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.java
@@ -0,0 +1,36 @@
+class Solution {
+
+    public int numberOfStableArrays(int zero, int one, int limit) {
+        final long MOD = 1000000007;
+        long[][][] dp = new long[zero + 1][one + 1][2];
+        for (int i = 0; i <= Math.min(zero, limit); i++) {
+            dp[i][0][0] = 1;
+        }
+        for (int j = 0; j <= Math.min(one, limit); j++) {
+            dp[0][j][1] = 1;
+        }
+        for (int i = 1; i <= zero; i++) {
+            for (int j = 1; j <= one; j++) {
+                if (i > limit) {
+                    dp[i][j][0] =
+                        dp[i - 1][j][0] +
+                        dp[i - 1][j][1] -
+                        dp[i - limit - 1][j][1];
+                } else {
+                    dp[i][j][0] = dp[i - 1][j][0] + dp[i - 1][j][1];
+                }
+                dp[i][j][0] = ((dp[i][j][0] % MOD) + MOD) % MOD;
+                if (j > limit) {
+                    dp[i][j][1] =
+                        dp[i][j - 1][1] +
+                        dp[i][j - 1][0] -
+                        dp[i][j - limit - 1][0];
+                } else {
+                    dp[i][j][1] = dp[i][j - 1][1] + dp[i][j - 1][0];
+                }
+                dp[i][j][1] = ((dp[i][j][1] % MOD) + MOD) % MOD;
+            }
+        }
+        return (int) ((dp[zero][one][0] + dp[zero][one][1]) % MOD);
+    }
+}
