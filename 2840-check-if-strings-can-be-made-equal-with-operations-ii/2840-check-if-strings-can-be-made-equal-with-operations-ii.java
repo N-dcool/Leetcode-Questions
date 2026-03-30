49dce93d4481e5e29c0e23b36d4047637f405ad0
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-03-30 23:24:15 +0530

    Time: 6 ms (61.84%), Space: 47.8 MB (85.53%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+
+    public boolean checkStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        int[] count1 = new int[256];
+        int[] count2 = new int[256];
+
+        for (int i = 0; i < s1.length(); i++) {
+            int offset = (i & 1) << 7;
+            count1[offset + s1.charAt(i)]++;
+            count2[offset + s2.charAt(i)]++;
+        }
+
+        return Arrays.equals(count1, count2);
+    }
+}
