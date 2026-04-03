c1fa15fbdab92fb9e52ce09cc7b9f5672232d5b0
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-04-03 23:00:14 +0530

    Time: 235 ms (13.33%), Space: 160.7 MB (40%) - LeetHub

---
+++ 3661-maximum-walls-destroyed-by-robots/3661-maximum-walls-destroyed-by-robots.java
@@ -0,0 +1,105 @@
+class Solution {
+
+    public int maxWalls(int[] robots, int[] distance, int[] walls) {
+        int n = robots.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] num = new int[n];
+        Map<Integer, Integer> robotsToDistance = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            robotsToDistance.put(robots[i], distance[i]);
+        }
+
+        Arrays.sort(robots);
+        Arrays.sort(walls);
+
+        for (int i = 0; i < n; i++) {
+            int pos1 = upperBound(walls, robots[i]);
+
+            int leftPos = 0;
+            if (i >= 1) {
+                int leftBound = Math.max(
+                    robots[i] - robotsToDistance.get(robots[i]),
+                    robots[i - 1] + 1
+                );
+                leftPos = lowerBound(walls, leftBound);
+            } else {
+                leftPos = lowerBound(
+                    walls,
+                    robots[i] - robotsToDistance.get(robots[i])
+                );
+            }
+            left[i] = pos1 - leftPos;
+
+            int rightPos = 0;
+            if (i < n - 1) {
+                int rightBound = Math.min(
+                    robots[i] + robotsToDistance.get(robots[i]),
+                    robots[i + 1] - 1
+                );
+                rightPos = upperBound(walls, rightBound);
+            } else {
+                rightPos = upperBound(
+                    walls,
+                    robots[i] + robotsToDistance.get(robots[i])
+                );
+            }
+            int pos2 = lowerBound(walls, robots[i]);
+            right[i] = rightPos - pos2;
+
+            if (i == 0) {
+                continue;
+            }
+            int pos3 = lowerBound(walls, robots[i - 1]);
+            num[i] = pos1 - pos3;
+        }
+
+        int subLeft = left[0];
+        int subRight = right[0];
+        for (int i = 1; i < n; i++) {
+            int currentLeft = Math.max(
+                subLeft + left[i],
+                subRight -
+                right[i - 1] +
+                Math.min(left[i] + right[i - 1], num[i])
+            );
+            int currentRight = Math.max(
+                subLeft + right[i],
+                subRight + right[i]
+            );
+            subLeft = currentLeft;
+            subRight = currentRight;
+        }
+
+        return Math.max(subLeft, subRight);
+    }
+
+    private int lowerBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+
+    private int upperBound(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (arr[mid] <= target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
