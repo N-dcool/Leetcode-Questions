0ee3f253e1de4bf6e618c19946ddd286b46e0097
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-04-21 23:20:07 +0530

    Time: 52 ms (49.46%), Space: 116 MB (52.69%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,64 @@
+class Solution {
+
+    private int[] fa;
+    private int[] rank;
+
+    private int find(int x) {
+        if (fa[x] != x) {
+            fa[x] = find(fa[x]);
+        }
+        return fa[x];
+    }
+
+    private void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+        if (x == y) return;
+        if (rank[x] < rank[y]) {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+        fa[y] = x;
+        if (rank[x] == rank[y]) {
+            rank[x]++;
+        }
+    }
+
+    public int minimumHammingDistance(
+        int[] source,
+        int[] target,
+        int[][] allowedSwaps
+    ) {
+        int n = source.length;
+        fa = new int[n];
+        rank = new int[n];
+        for (int i = 0; i < n; i++) {
+            fa[i] = i;
+        }
+
+        for (int[] pair : allowedSwaps) {
+            union(pair[0], pair[1]);
+        }
+
+        Map<Integer, Map<Integer, Integer>> sets = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int f = find(i);
+            sets.putIfAbsent(f, new HashMap<>());
+            Map<Integer, Integer> cnt = sets.get(f);
+            cnt.put(source[i], cnt.getOrDefault(source[i], 0) + 1);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int f = find(i);
+            Map<Integer, Integer> cnt = sets.get(f);
+            if (cnt.getOrDefault(target[i], 0) > 0) {
+                cnt.put(target[i], cnt.get(target[i]) - 1);
+            } else {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
