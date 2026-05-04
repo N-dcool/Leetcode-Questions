2b6881bb31642da8197c27939915066b435507f7
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-04 23:43:12 +0530

    Time: 0 ms (100%), Space: 43.9 MB (33.41%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,13 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length, k = n - 1;
+        for (int i = 0; i < n >> 1; i++)
+            for (int j = i; j < k - i; j++) {
+                int t = mat[i][j];
+                mat[i][j] = mat[k - j][i];
+                mat[k - j][i] = mat[k - i][k - j];
+                mat[k - i][k - j] = mat[j][k - i];
+                mat[j][k - i] = t;
+            }
+    }
+}
