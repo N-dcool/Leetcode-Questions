ad3442b49fd5643e0b2a6cd10173c0072241d537
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-13 21:42:50 +0530

    Time: 429 ms (24.03%), Space: 0B (100%) - LeetHub

--- 0178-rank-scores/0178-rank-scores.sql
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -1,5 +1,3 @@
 /* Write your PL/SQL query statement below */
 
-
-SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as "rank" 
-    FROM Scores;
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) as rank FROM Scores;
