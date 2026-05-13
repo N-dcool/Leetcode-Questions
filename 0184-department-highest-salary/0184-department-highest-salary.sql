ee720e801cb71d5ee3bdd1c89dff509565404a86
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-14 02:45:40 +0530

    Time: 632 ms (80.68%), Space: 0B (100%) - LeetHub

--- 0184-department-highest-salary/0184-department-highest-salary.sql
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -4,6 +4,6 @@ select Department, Employee, salary
     from(
         select d.name as  Department , e.name as Employee, e.salary,
             DENSE_RANK() over(partition by e.departmentId order by e.salary DESC) as rnk 
-            from Employee e left join Department d
+            from Employee e inner join Department d
                 on e.departmentId = d.id
     ) where rnk = 1; 
