5da634fa7c3cd8d907ed8fae082cba61ab3e3033
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-13 20:15:39 +0530

    Time: 406 ms (26.53%), Space: 0B (100%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,6 +1,6 @@
 /* Write your PL/SQL query statement below */
 SELECT COALESCE(
-    (SELECT MAX(salary) 
+    (SELECT DISTINCT salary 
      FROM (
          SELECT salary, DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
          FROM Employee
