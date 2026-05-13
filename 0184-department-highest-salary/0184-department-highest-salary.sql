5c834454ff011fe91ef9edaa72ab1ad6fb118700
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-14 02:42:49 +0530

    Time: 650 ms (72.1%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,9 @@
+/* Write your PL/SQL query statement below */
+
+select Department, Employee, salary 
+    from(
+        select d.name as  Department , e.name as Employee, e.salary,
+            DENSE_RANK() over(partition by e.departmentId order by e.salary DESC) as rnk 
+            from Employee e left join Department d
+                on e.departmentId = d.id;
+    ) where rnk = 1; 
