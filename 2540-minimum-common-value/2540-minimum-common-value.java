f818fa6db33abbc7905234a3ff1eefb425b2d50f
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-05-20 00:57:27 +0530

    Time: 2 ms (88.98%), Space: 80.4 MB (63.48%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.java
+++ 2540-minimum-common-value/2540-minimum-common-value.java
@@ -1,11 +1,22 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
 
-        for(int num : nums1){
-            if(binarySearch(nums2, num) != -1){
-                return num;
-            }
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int n1 = 0;
+        int n2 = 0;
+        while(n1<n && n2<m){
+            if(nums1[n1] == nums2[n2]) return nums1[n1];
+            else if(nums1[n1] > nums2[n2]) n2++;
+            else n1++;
         }
+
+        // for(int num : nums1){
+        //     if(binarySearch(nums2, num) != -1){
+        //         return num;
+        //     }
+        // }
         
         return -1;
     }
