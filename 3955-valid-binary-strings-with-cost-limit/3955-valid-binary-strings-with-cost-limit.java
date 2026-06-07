f9bf0f33cee0ce623dfa2e3b7072614e5d00e424
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-06-07 20:55:49 +0530

    Time: 4 ms (100%), Space: 47.4 MB (55.16%) - LeetHub

--- 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
+++ 3955-valid-binary-strings-with-cost-limit/3955-valid-binary-strings-with-cost-limit.java
@@ -1,28 +1,26 @@
 class Solution {
     List<String> res = new ArrayList<>();
     public List<String> generateValidStrings(int n, int k) {
-        solve(1, new StringBuilder("0"), n, k);
-        solve(1, new StringBuilder("1"), n, k);
+
+        solve(0, -1, new char[n], n, k);
 
         return res;
     }
 
-    public void solve(int i, StringBuilder sb, int n, int k) {
+    public void solve(int i, int prevBit, char[] binary, int n, int k) {
+        if(k<0) return;
         if(i==n) {
             if(k >= 0) {
-                res.add(sb.toString());
+                res.add(new String(binary));
             }
             return;
         }
 
-        char prev = sb.charAt(i-1);
-
-        if(prev == '1'){
-            solve(i+1, sb.append("0"), n, k);
-        } else{
-            StringBuilder newSb = new StringBuilder(sb);
-            solve(i+1, newSb.append("0"), n, k);
-            solve(i+1, sb.append("1"), n, k-i);
-        }
+        binary[i] = '0';
+        solve(i+1, 0 , binary, n, k);
+        if(prevBit != 1){
+            binary[i] = '1';
+            solve(i+1, 1, binary, n, k-i);
+        } 
     }
 }
