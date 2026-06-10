8dcb719dbe65b8752009716a32747cd5ef4bccf0
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-06-10 15:53:38 +0530

    Time: 266 ms (59.38%), Space: 280.7 MB (6.25%) - LeetHub

--- 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.java
+++ 3691-maximum-total-subarray-value-ii/3691-maximum-total-subarray-value-ii.java
@@ -1,4 +1,86 @@
 class Solution {
+
+    private record Range(int i, int j, int val) { }
+
+    public long maxTotalValue(int[] nums, int k) {
+        int n = nums.length;
+        SparseTable st = new SparseTable(nums);
+
+        PriorityQueue<Range> pq = new PriorityQueue<>(
+            (a,b) -> {
+                return a.val() == b.val() ? 
+                        Integer.compare(a.i(), b.i()) :
+                         Integer.compare(b.val(), a.val());
+            }
+        );
+
+        for(int i=0; i<n-1; i++) {
+            pq.add(new Range(i, n-1, st.query(i, n-1)));
+        }
+
+        long res = 0;
+
+        while(!pq.isEmpty() && k-->0) {
+            Range cur = pq.poll();
+            int i = cur.i();
+            int j = cur.j();
+            res += cur.val();
+
+            if( i < j-1) pq.add(new Range(i, j-1, st.query(i,j-1)));
+        }
+
+        return res;
+    }
+}
+
+class SparseTable {
+    private record Pair(int min, int max) { }
+    private final Pair[][] st;
+    private final int[] log;
+
+    public SparseTable(int[] arr) {
+        int n = arr.length;
+        log = new int[n+1];
+        for(int i=2; i<=n; i++) {
+            log[i] = log[i/2] + 1;
+        }
+
+        int m = log[n] + 1;
+        st = new Pair[n][m];
+
+        for(int i=0; i<n; i++) {
+            st[i][0] = new Pair(arr[i], arr[i]);
+        }
+
+        for(int k=1; k<m; k++) {
+            int len = 1<<k;
+            int half = len>>1;
+
+            for(int i=0; i+len <= n; i++) {
+
+                int min = Math.min(st[i][k-1].min(), st[i+half][k-1].min());
+                int max = Math.max(st[i][k-1].max(), st[i+half][k-1].max());
+
+                st[i][k] = new Pair(min, max);
+            }
+        }
+
+    }
+
+    public int query(int l, int r) {
+        int len = r-l+1;
+        int k = log[len];
+
+        int min = Math.min(st[l][k].min(), st[r-(1<<k)+1][k].min()); 
+        int max = Math.max(st[l][k].max(), st[r-(1<<k)+1][k].max()); 
+
+        return max-min;
+    }
+}
+
+// SegmentTree:
+/*
+class Solution {
     private record Range(int i, int j, int val) { }
 
     public long maxTotalValue(int[] nums, int k) {
@@ -90,6 +172,7 @@ class SegmentTree {
         tree[node] = new Pair(Math.min(tree[leftNode].min, tree[rightNode].min), Math.max(tree[leftNode].max, tree[rightNode].max));
     }
 }
+*/
 
 // brute force:
 /*
