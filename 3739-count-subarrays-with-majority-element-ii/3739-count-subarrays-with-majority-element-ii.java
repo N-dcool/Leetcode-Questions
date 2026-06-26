1617e7b7ace77940e8e97b0618fe1b52013adc2d
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-06-26 23:20:37 +0530

    Time: 2 ms (100%), Space: 88 MB (72.13%) - LeetHub

---
+++ 3739-count-subarrays-with-majority-element-ii/3739-count-subarrays-with-majority-element-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public long countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        // represents the occurrence count of prefix sums -n, -(n-1), ..., 0, 1, ..., n, with index offset by n.
+        int[] pre = new int[n * 2 + 1];
+        pre[n] = 1;
+        int cnt = n;
+        long ans = 0;
+        long presum = 0;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] == target) {
+                presum += pre[cnt];
+                ++cnt;
+                ++pre[cnt];
+            } else {
+                --cnt;
+                presum -= pre[cnt];
+                ++pre[cnt];
+            }
+            ans += presum;
+        }
+        return ans;
+    }
+}
