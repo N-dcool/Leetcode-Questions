981235111499dba120862649da3cc747578f3af4
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-07-30 23:01:34 +0530

    Time: 3 ms (11.24%), Space: 42.8 MB (97.85%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n = word.length();
+        if(n<=8) return n;
+
+        int unique = 0;
+        boolean[] letter = new boolean[26];
+        for(char c : word.toCharArray()) {
+            if(!letter[c-'a']){
+                unique++;
+                letter[c-'a'] = true;
+            }
+        }
+
+        System.out.println(unique);
+
+        if(unique<=16) return 8 + (unique-8)*2;
+
+        if(unique<=24) return 8 + 8*2 + (unique-16)*3;
+
+        return 8 + 8*2 + 8*3 + (unique-24)*4;
+    }
+}
