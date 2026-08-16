a3efe4f25ffd6c06f18914d11cc1b092ff7e7658
Naresh Choudhary <107427909+N-dcool@users.noreply.github.com>
2026-08-16 22:58:36 +0530

    Time: 7 ms (29.05%), Space: 114.6 MB (94.59%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean stoneGameIX(int[] stones) {
+        int cnt0 = 0,
+            cnt1 = 0,
+            cnt2 = 0;
+        for (int val : stones) {
+            int type = val % 3;
+            if (type == 0) {
+                ++cnt0;
+            } else if (type == 1) {
+                ++cnt1;
+            } else {
+                ++cnt2;
+            }
+        }
+        if (cnt0 % 2 == 0) {
+            return cnt1 >= 1 && cnt2 >= 1;
+        }
+        return cnt1 - cnt2 > 2 || cnt2 - cnt1 > 2;
+    }
+}
